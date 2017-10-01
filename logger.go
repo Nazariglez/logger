539b13c02fd01acc0c6dddbd3c2810bec5d2c920
Nazarí González <nazari.nz@gmail.com>
2017-10-02 00:08:54 +0100

    added a basic structure

--- logger.go
+++ logger.go
@@ -2,7 +2,314 @@
 
 package logger
 
-type Logger struct {
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"sync"
+	"time"
+)
 
+type LogLevel int
+
+const (
+	TRACE LogLevel = iota
+	DEBUG
+	INFO
+	LOG
+	WARN
+	ERROR
+	FATAL
+)
+
+type Logger struct {
+	mu sync.Mutex
+
+	logger    *log.Logger
+	loggerErr *log.Logger
+
+	level LogLevel
+
+	fileLevel  LogLevel
+	fileTime   time.Time
+	filePath   string
+	fileName   string
+	fileLogger *log.Logger
+	logFile    *os.File
 }
 
+func New() *Logger {
+	flags := log.Flags()
+
+	return &Logger{
+		level:     LOG,
+		logger:    log.New(os.Stdout, "", flags),
+		loggerErr: log.New(os.Stderr, "", flags),
+	}
+}
+
+func (l *Logger) SetLevel(level LogLevel) *Logger {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.level = level
+	return l
+}
+
+func (l *Logger) GetLevel() LogLevel {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	return l.level
+}
+
+func (l *Logger) EnableFileOutput(name, directory string, level LogLevel) error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.fileName = name
+	l.filePath = directory
+	l.fileLevel = level
+
+	err := l.checkCurrentFile()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+func (l *Logger) DisableFileOutput() error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	if l.logFile != nil {
+		err := l.logFile.Close()
+		if err != nil {
+			return err
+		}
+
+		l.logFile = nil
+	}
+
+	return nil
+}
+
+func (l *Logger) checkCurrentFile() error {
+	now := time.Now()
+
+	if l.logFile != nil {
+
+		//create a new file for the new day
+		if now.Day() != l.fileTime.Day() {
+			err := l.logFile.Close()
+			if err != nil {
+				return err
+			}
+
+			err = l.initLogFile()
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}
+
+	//initialize logFile
+	err := l.initLogFile()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (l *Logger) initLogFile() error {
+	now := time.Now()
+
+	name := l.fileName + "." + now.Format("02012006") + ".log"
+	f, err := os.OpenFile(
+		path.Join(l.filePath, name),
+		os.O_WRONLY|os.O_CREATE|os.O_APPEND,
+		0640,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	l.logFile = f
+	l.fileTime = now
+
+	l.fileLogger = log.New(f, "", log.Flags())
+
+	return nil
+}
+
+func (l *Logger) msg(text string, level LogLevel, isErr bool) {
+	if l.level <= level {
+		if isErr {
+			l.loggerErr.Println(getPrefix(level, false) + text)
+		} else {
+			l.logger.Println(getPrefix(level, false) + text)
+		}
+	}
+
+	if l.logFile != nil && l.fileLevel <= level {
+		err := l.checkCurrentFile()
+
+		if err != nil {
+			l.logger.Println(getPrefix(WARN, false) + "CANNOT SAVE LOG TO FILE - " + err.Error())
+			return
+		}
+
+		l.fileLogger.Println(getPrefix(level, true) + text)
+	}
+}
+
+func (l *Logger) Trace(text string) *Logger {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.msg(text, TRACE, false)
+	return l
+}
+
+func (l *Logger) Tracef(format string, v ...interface{}) *Logger {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.msg(fmt.Sprintf(format, v...), TRACE, false)
+	return l
+}
+
+func (l *Logger) Debug(text string) *Logger {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.msg(text, DEBUG, false)
+	return l
+}
+
+func (l *Logger) Debugf(format string, v ...interface{}) *Logger {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.msg(fmt.Sprintf(format, v...), DEBUG, false)
+	return l
+}
+
+func (l *Logger) Info(text string) *Logger {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.msg(text, INFO, false)
+	return l
+}
+
+func (l *Logger) Infof(format string, v ...interface{}) *Logger {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.msg(fmt.Sprintf(format, v...), INFO, false)
+	return l
+}
+
+func (l *Logger) Log(text string) *Logger {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.msg(text, LOG, false)
+	return l
+}
+
+func (l *Logger) Logf(format string, v ...interface{}) *Logger {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.msg(fmt.Sprintf(format, v...), LOG, false)
+	return l
+}
+
+func (l *Logger) Warn(text string) *Logger {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.msg(text, WARN, false)
+	return l
+}
+
+func (l *Logger) Warnf(format string, v ...interface{}) *Logger {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.msg(fmt.Sprintf(format, v...), WARN, false)
+	return l
+}
+
+func (l *Logger) Error(text string) *Logger {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.msg(text, ERROR, true)
+	return l
+}
+
+func (l *Logger) Errorf(format string, v ...interface{}) *Logger {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.msg(fmt.Sprintf(format, v...), ERROR, true)
+	return l
+}
+
+func (l *Logger) Fatal(text string) *Logger {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.msg(text, FATAL, true)
+	return l
+}
+
+func (l *Logger) Fatalf(format string, v ...interface{}) *Logger {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.msg(fmt.Sprintf(format, v...), FATAL, true)
+	return l
+}
+
+//
+var levelPrefix = map[LogLevel]string{
+	TRACE: "Trace: ",
+	DEBUG: "Debug: ",
+	INFO:  "Info: ",
+	LOG:   "Log: ",
+	WARN:  "Warn: ",
+	ERROR: "Error: ",
+	FATAL: "Fatal: ",
+}
+
+func getPrefix(level LogLevel, file bool) string {
+	prefix := levelPrefix[level]
+	if file {
+		return prefix
+	}
+
+	switch level {
+	case TRACE:
+		prefix = "\033[35m" + prefix + "\033[39m"
+	case DEBUG:
+		prefix = "\033[96m" + prefix + "\033[39m"
+	case INFO:
+		prefix = "\033[32m" + prefix + "\033[39m"
+	case WARN:
+		prefix = "\033[93m" + prefix + "\033[39m"
+	case ERROR:
+		prefix = "\033[31m" + prefix + "\033[39m"
+	case FATAL:
+		prefix = "\033[0;41m" + prefix + "\033[0;39m"
+	}
+
+	return prefix
+}
