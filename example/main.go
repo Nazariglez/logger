c6c79bca1b53d59395cf9b75031f536fbe550ed4
Nazarí González <nazari.nz@gmail.com>
2017-10-02 16:34:37 +0100

    formatted with go fmt

--- example/main.go
+++ example/main.go
@@ -3,43 +3,43 @@
 package main
 
 import (
-  "github.com/nazariglez/logger"
-  "fmt"
+	"fmt"
+	"github.com/nazariglez/logger"
 )
 
 func main() {
-  //loggerExample()
-  asyncLoggerExample()
+	//loggerExample()
+	asyncLoggerExample()
 }
 
 func loggerExample() {
-  l := logger.New()
-  l.SetLevel(logger.DEBUG) //print in the terminal info level logs
-  l.EnableFileOutput("test", "./", logger.TRACE) //print in the file trace level logs
+	l := logger.New()
+	l.SetLevel(logger.DEBUG)                       //print in the terminal info level logs
+	l.EnableFileOutput("test", "./", logger.TRACE) //print in the file trace level logs
 
-  l.Trace("Find me in the log file")
+	l.Trace("Find me in the log file")
 
-  l.Debug("Hey i'm a debug message")
-  l.Info("Hey i'm a debug message")
-  l.Log("Hey i'm a normal message")
-  l.Warn("Hey i'm a warn message")
-  l.Error("Hey i'm a error message")
-  l.Fatal("Hey i'm a fatal message")
+	l.Debug("Hey i'm a debug message")
+	l.Info("Hey i'm a debug message")
+	l.Log("Hey i'm a normal message")
+	l.Warn("Hey i'm a warn message")
+	l.Error("Hey i'm a error message")
+	l.Fatal("Hey i'm a fatal message")
 }
 
 func asyncLoggerExample() {
-  l := logger.NewAsync() //async logger
-  l.SetLevel(logger.TRACE)
-  l.EnableFileOutput("test", "./", logger.TRACE)
+	l := logger.NewAsync() //async logger
+	l.SetLevel(logger.TRACE)
+	l.EnableFileOutput("test", "./", logger.TRACE)
 
-  l.Trace("Find me in the log file")
+	l.Trace("Find me in the log file")
 
-  fmt.Println("I'm the first message in your terminal not the 2nd!")
+	fmt.Println("I'm the first message in your terminal not the 2nd!")
 
-  l.Debug("Hey i'm a debug message")
-  l.Info("Hey i'm a debug message")
-  l.Log("Hey i'm a normal message")
-  l.Warn("Hey i'm a warn message")
-  l.Error("Hey i'm a error message")
-  l.Fatal("Hey i'm a fatal message")
+	l.Debug("Hey i'm a debug message")
+	l.Info("Hey i'm a debug message")
+	l.Log("Hey i'm a normal message")
+	l.Warn("Hey i'm a warn message")
+	l.Error("Hey i'm a error message")
+	l.Fatal("Hey i'm a fatal message")
 }
